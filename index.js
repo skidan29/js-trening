c924cac43acb224352459cd2ef0f86d2da3374e8
aleksandr_skidan@rntgroup.com <aleksandr_skidan@rntgroup.com>
2024-02-19 11:54:44 +0300

    11

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('started index.ts');
+var sum = function (a, b) { return Number(a) + Number(b); };
+var obj = {
+    name: 'name',
+    age: 30,
+    primarySkill: 'Developer',
+};
+console.log(obj);
